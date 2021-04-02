fbbafb9dd6bb0e570c5769f756afd56b12e793cf
Victor Gallegos <316160456@ciencias.unam.mx>
2021-04-01 23:17:52 -0600

    Clase para representar nodos

---
+++ Estructuras de Datos con Java/3.-Listas Enlazadas/Nodo/src/Nodo.java
@@ -0,0 +1,20 @@
+
+public class Nodo {
+int dato;
+Nodo referencia;
+
+//Definir constructor
+public Nodo (int d) {
+        dato = d;
+}
+
+public int getDato(){
+        return dato;
+}
+public int setRef(Nodo referencia){
+        ref = referencia;
+}
+public void getRef(){
+        return ref;
+}
+}
