ecae2cc4e49ab3105bdc2a850599ea39c8dc9f96
Victor Gallegos <316160456@ciencias.unam.mx>
2021-08-30 00:02:11 -0500

    Interfaces funcionales

---
+++ Curso de programación funcional en Java/2.-Expresiones lambda/src/interfaces_funcionales/ISaludar.java
@@ -0,0 +1,8 @@
+package interfaces_funcionales;
+
+@FunctionalInterface
+public interface ISaludar {
+
+    void Saluda();
+
+}
---
+++ Curso de programación funcional en Java/2.-Expresiones lambda/src/interfaces_funcionales/Main.java
@@ -0,0 +1,12 @@
+package interfaces_funcionales;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ISaludar saludar = () -> {
+            System.out.println("Hola mundo");
+        };
+        saludar.Saluda();
+        ;
+    }
+}
