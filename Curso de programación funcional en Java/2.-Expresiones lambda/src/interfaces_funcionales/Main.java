ded4b43ad8893960678f0440b5fc01deda257e3d
Victor Gallegos <316160456@ciencias.unam.mx>
2021-08-30 01:08:53 -0500

    Ejecutar sentencias multiples

--- Curso de programación funcional en Java/2.-Expresiones lambda/src/interfaces_funcionales/Main.java
+++ Curso de programación funcional en Java/2.-Expresiones lambda/src/interfaces_funcionales/Main.java
@@ -8,8 +8,14 @@ public class Main {
         // Siempre que tengamos un solo parametro podemos omitir el uso de parentesis
         // Siempre que se ejecute una unica sentencia podemos omitir el uso de llaves
         ISaludar saludar = nombre -> System.out.println("Hola mundo " + nombre);
-        ;
         saludar.Saluda("Hugo");
-        ;
+        ISaludar saludarDos = nombre -> {
+            if (nombre.equals("Hugo")) {
+                System.out.println("Hola Hugo, eres mi tocayo");
+            } else {
+                System.out.println("Hola mundo " + nombre);
+            }
+        };
+        saludarDos.Saluda("Hugo 2");
     }
 }
