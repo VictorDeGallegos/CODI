490fbe8d35fead9d8e22198b3766b5be1d99f0c3
Victor Gallegos <316160456@ciencias.unam.mx>
2021-08-30 23:40:33 -0500

    Transformar valores

--- Curso de programación funcional en Java/3.-Api Stream/src/main/Main.java
+++ Curso de programación funcional en Java/3.-Api Stream/src/main/Main.java
@@ -6,17 +6,27 @@ import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
+
+        // Obtener el cuadrado de todos los numeros en la lista.
+        List<Integer> numeros = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10); // 1er lista
+
+        List<Integer> result = numeros.stream() // 2da lista
+                .map(num -> num * num) // 3era lista
+                .collect(Collectors.toList()); // 4ta lista
+        System.out.println(numeros);
+        System.out.println(result);
+
         // Filtrar!
         // Obtener la cantidad de usuarios con edad mayor a 18 y que el username
         // comience con R
 
-        List<User> users = new ArrayList<>();
+        // List<User> users = new ArrayList<>();
 
-        users.add(new User("Rafael", 25));
-        users.add(new User("Daniel", 25));
-        users.add(new User("Tania", 15));
-        users.add(new User("Luisa", 24));
-        users.add(new User("Raul", 21));
+        // users.add(new User("Rafael", 25));
+        // users.add(new User("Daniel", 25));
+        // users.add(new User("Tania", 15));
+        // users.add(new User("Luisa", 24));
+        // users.add(new User("Raul", 21));
 
         // long cantidad = users.stream().filter(user -> user.getAge() > 18)
         // .filter(user -> user.getUsername().startsWith("R")).count();
@@ -33,7 +43,8 @@ public class Main {
         // Filter -> Abstraccion -Nuevo stream
         // System.out.println(stream.filter(user -> user.getAge() > 18).count());
 
-        List<User> newUsers = users.stream().filter(user -> user.getAge() > 18).collect(Collectors.toList());
-        System.out.println(newUsers);
+        // List<User> newUsers = users.stream().filter(user -> user.getAge() >
+        // 18).collect(Collectors.toList());
+        // System.out.println(newUsers);
     }
 }
